fde3b37da52af7d00e8f29a734a33ca363a8fb3c
Santapan <techysanta0169@gmail.com>
2024-06-23 23:59:27 +0530

    backend

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -34,7 +34,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/Santapanghavidel'
+                  href='https://www.github.com/santapan345'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
@@ -63,7 +63,7 @@ export default function FooterCom() {
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/Santapanghavidel' icon={BsGithub}/>
+            <Footer.Icon href='https://github.com/santapan345' icon={BsGithub}/>
             <Footer.Icon href='#' icon={BsDribbble}/>
 
           </div>
